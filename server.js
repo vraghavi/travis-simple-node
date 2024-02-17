86f75d452a57d947c9819218a432b5d718acc395
vraghavi <vannalaraghavi@gmail.com>
2024-02-16 20:25:50 -0600

    new env var

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
     res.send('Hello World!');
 });
 app.listen(8080, function () {
